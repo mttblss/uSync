b12bb9fca7679a70bd1bc08766e86fb0227448a2
Kevin Jump <kevin@thejumps.co.uk>
2024-11-15 16:55:27 +0000

    HACK: put a small pause in  , to stop Umbraco tripping up on itself.

--- uSync.BackOffice/SyncHandlers/Handlers/ContentTypeBaseHandler.cs
+++ uSync.BackOffice/SyncHandlers/Handlers/ContentTypeBaseHandler.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
 
+using System.Threading;
+using System.Threading.Tasks;
+
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Models.Entities;
+using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Strings;
 
@@ -32,5 +36,13 @@ public abstract class ContentTypeBaseHandler<TObject> : SyncHandlerContainerBase
         ISyncItemFactory syncItemFactory)
         : base(logger, entityService, appCaches, shortStringHelper, syncFileService, mutexService, uSyncConfig, syncItemFactory)
     { }
+
+    public override async Task HandleAsync(SavingNotification<TObject> notification, CancellationToken cancellationToken)
+    {
+        // this is a hack fix, while we look into it, 
+        // deeply suspect there is a lock in the core :( 
+        Thread.Sleep(75);
+        await base.HandleAsync(notification, cancellationToken);
+    }
 
 }
